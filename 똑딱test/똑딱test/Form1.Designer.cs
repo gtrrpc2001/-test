79f83095c73fd520ba8cfa58585bbd470333b4f1
gtrrpc2001 <cbdgjs3548@naver.com>
2023-09-05 15:06:19 +0900

    commit

---
+++ 똑딱test/똑딱test/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace 똑딱test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다.
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마십시오.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnOff = new System.Windows.Forms.Button();
+            this.btnOn = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.cmbPortName = new System.Windows.Forms.ComboBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.SuspendLayout();
+            // 
+            // btnOff
+            // 
+            this.btnOff.Location = new System.Drawing.Point(113, 95);
+            this.btnOff.Name = "btnOff";
+            this.btnOff.Size = new System.Drawing.Size(95, 40);
+            this.btnOff.TabIndex = 12;
+            this.btnOff.Text = "꺼짐";
+            this.btnOff.UseVisualStyleBackColor = true;
+            this.btnOff.Click += new System.EventHandler(this.btnOff_Click);
+            // 
+            // btnOn
+            // 
+            this.btnOn.Location = new System.Drawing.Point(12, 95);
+            this.btnOn.Name = "btnOn";
+            this.btnOn.Size = new System.Drawing.Size(95, 40);
+            this.btnOn.TabIndex = 13;
+            this.btnOn.Text = "켜짐";
+            this.btnOn.UseVisualStyleBackColor = true;
+            this.btnOn.Click += new System.EventHandler(this.btnOn_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(113, 49);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(95, 40);
+            this.btnClose.TabIndex = 11;
+            this.btnClose.Text = "연결끊기";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(12, 49);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(95, 40);
+            this.btnConnect.TabIndex = 10;
+            this.btnConnect.Text = "연결";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // cmbPortName
+            // 
+            this.cmbPortName.FormattingEnabled = true;
+            this.cmbPortName.Location = new System.Drawing.Point(12, 12);
+            this.cmbPortName.Name = "cmbPortName";
+            this.cmbPortName.Size = new System.Drawing.Size(196, 20);
+            this.cmbPortName.TabIndex = 9;
+            this.cmbPortName.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.cmbPortName_KeyPress);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(226, 156);
+            this.Controls.Add(this.btnOff);
+            this.Controls.Add(this.btnOn);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.cmbPortName);
+            this.Name = "Form1";
+            this.Text = "test";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOff;
+        private System.Windows.Forms.Button btnOn;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.ComboBox cmbPortName;
+        private System.IO.Ports.SerialPort serialPort1;
+    }
+}
+
---
+++ 똑딱test/똑딱test/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace 똑딱test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cmbPortName.DataSource = SerialPort.GetPortNames();
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                serialPort1.PortName = cmbPortName.Text;                
+                serialPort1.StopBits = StopBits.One;                
+                serialPort1.RtsEnable = true;                
+                serialPort1.Open();
+                if (serialPort1.IsOpen)
+                    MessageBox.Show("포트가 연결 됬습니다.");
+                cmbPortName.Enabled = false;
+            }
+            catch
+            {
+                if (serialPort1.IsOpen)
+                {
+                    var message = "포트가 이미 열려 있습니다. 연결을 취소 하시겠습니까? ";
+                    DisConnect(message);
+                }
+                else
+                {
+                    return;
+                }
+
+            }
+
+            finally
+            {
+
+            }
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            if (PortIsOpen() == false)                
+                return;
+            var message = "연결을 취소 하시겠습니까?";
+            DisConnect(message);
+        }
+
+        private void DisConnect(string message)
+        {
+            if (PortIsOpen() == false)
+                return;
+
+            var result = MessageBox.Show(message, "", MessageBoxButtons.YesNo);
+            if (result == DialogResult.Yes)
+            {
+                serialPort1.Close();
+                cmbPortName.Enabled = true;
+            }
+        }
+
+        private void btnOn_Click(object sender, EventArgs e)
+        {
+            if (PortIsOpen() == false)
+                return;
+            byte[] hex = { 0xA0, 0x01, 0x01, 0xA2 };
+            serialPort1.Write(hex, 0, 4);
+        }
+
+        private void btnOff_Click(object sender, EventArgs e)
+        {
+            if (PortIsOpen() == false)
+                return;
+            byte[] hex = { 0xA0, 0x01, 0x00, 0xA1 };
+            serialPort1.Write(hex, 0, 4);
+        }
+
+        private bool PortIsOpen() 
+        {
+            if (serialPort1.IsOpen == false)
+                MessageBox.Show("연결된 포트가 없습니다.");
+            return serialPort1.IsOpen;
+        }
+
+        private void cmbPortName_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = true;
+        }
+
+    }
+}
